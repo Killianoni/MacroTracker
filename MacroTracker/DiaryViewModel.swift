969b3751b28893938ff67f51fcfa03675a81ae75
Killianoni <a.killian@hotmail.fr>
2024-03-11 19:00:18 +0100

    Macros does not appear on first start (#3)
    * fixed progressbar infinite width
    
    * SwiftData working lmao

--- MacroTracker/DiaryViewModel.swift
+++ MacroTracker/DiaryViewModel.swift
@@ -15,54 +15,45 @@ class DiaryViewModel: ObservableObject {
         static let quickAddMinimalNumber: CGFloat = 0
     }
     
-    @Published var incrementCarbs: CGFloat
-    @Published var incrementProteins: CGFloat
-    @Published var incrementFat: CGFloat
-    @Published var dailyMacros: Macros?
+    @Published var incrementCarbs: CGFloat = 0
+    @Published var incrementProteins: CGFloat = 0
+    @Published var incrementFat: CGFloat = 0
+    
+    @Published var currentDate: Date = .now
     
     var context: ModelContext?
-    
-    var totalCarbs: CGFloat
-    var totalFat: CGFloat
-    var totalProteins: CGFloat
-    var totalCalories: CGFloat
-    
-    @Published var currentDate: Date
-    
-    init(incrementCarbs: CGFloat = 0, incrementProteins: CGFloat = 0, incrementFat: CGFloat = 0, dailyMacros: Macros? = nil, context: ModelContext? = nil, currentDate: Date = .now) {
-        self.incrementCarbs = incrementCarbs
-        self.incrementProteins = incrementProteins
-        self.incrementFat = incrementFat
-        self.dailyMacros = dailyMacros
-        self.context = context
         
-        self.totalCarbs = (UserDefaults.standard.string(forKey: "carbs")?.toCGFloat())!
-        self.totalFat = (UserDefaults.standard.string(forKey: "fat")?.toCGFloat())!
-        self.totalProteins = (UserDefaults.standard.string(forKey: "proteins")?.toCGFloat())!
-        self.totalCalories = (UserDefaults.standard.string(forKey: "calories")?.toCGFloat())!
-        
-        self.currentDate = currentDate
-        
-    }
+    var totalCarbs = (UserDefaults.standard.string(forKey: "carbs")?.toCGFloat())!
+    var totalFat = (UserDefaults.standard.string(forKey: "fat")?.toCGFloat())!
+    var totalProteins = (UserDefaults.standard.string(forKey: "proteins")?.toCGFloat())!
+    var totalCalories = (UserDefaults.standard.string(forKey: "calories")?.toCGFloat())!
     
-    func add() {
-        guard let macros = dailyMacros else {
+    
+    func isNewDay(macros: [Macros]) {
+        guard macros.first(where: { Calendar.current.isDate($0.date, equalTo: currentDate, toGranularity: .day) }) != nil else {
+            context?.insert(Macros(date: currentDate))
             return
         }
-        
-        if checkQuickEntrys() {
-            
-            macros.carbs += incrementCarbs
-            macros.fat += incrementFat
-            macros.proteins += incrementProteins
+    }
+    
+    func getMacros(macros: [Macros]) -> Macros? {
+        guard let macros = macros.first(where: { Calendar.current.isDate($0.date, equalTo: currentDate, toGranularity: .day) }) else {
+            return nil
+        }
+        return macros
+    }
+    
+    func add(macros: [Macros]) {
+            getMacros(macros: macros)?.carbs += incrementCarbs
+            getMacros(macros: macros)?.fat += incrementFat
+            getMacros(macros: macros)?.proteins += incrementProteins
             
             let caloriesFromCarbs = incrementCarbs * 4
             let caloriesFromFat = incrementFat * 9
             let caloriesFromProteins = incrementProteins * 4
             
-            macros.calories += caloriesFromCarbs + caloriesFromFat + caloriesFromProteins
+            getMacros(macros: macros)?.calories += caloriesFromCarbs + caloriesFromFat + caloriesFromProteins
             
-        }
         resetMacroIncrements()
     }
 
@@ -72,38 +63,21 @@ class DiaryViewModel: ObservableObject {
         incrementProteins = 0
     }
     
-    private func checkQuickEntrys() -> Bool {
-        guard let macros = dailyMacros,
-              macros.carbs + incrementCarbs >= Constants.quickAddMinimalNumber,
-                  macros.fat + incrementFat >= Constants.quickAddMinimalNumber,
-                  macros.proteins + incrementProteins >= Constants.quickAddMinimalNumber else {
-                return false
-            }
-        
-        guard let macros = dailyMacros,
-                  macros.carbs < Constants.quickAddMaxNumber,
-                  macros.fat < Constants.quickAddMaxNumber,
-                  macros.proteins < Constants.quickAddMaxNumber else {
-                return false
-            }
-        
-        return true
-    }
-    
-    private func insert(_ date: Date) {
-        guard context != nil else {
-            return
-        }
-        context!.insert(Macros(date: date))
-    }
-    
-    func isNewDay() {
-        if dailyMacros == nil {
-            insert(.now)
-        }
-    }
-    
-    func fetchMacros(_ macros: [Macros], _ date: Date) {
-        dailyMacros = macros.first(where: { Calendar.current.isDate($0.date, equalTo: date, toGranularity: .day) }) ?? nil
-    }
+//    private func checkQuickEntrys() -> Bool {
+//        guard let macros = dailyMacros,
+//              macros.carbs + incrementCarbs >= Constants.quickAddMinimalNumber,
+//                  macros.fat + incrementFat >= Constants.quickAddMinimalNumber,
+//                  macros.proteins + incrementProteins >= Constants.quickAddMinimalNumber else {
+//                return false
+//            }
+//        
+//        guard let macros = dailyMacros,
+//                  macros.carbs < Constants.quickAddMaxNumber,
+//                  macros.fat < Constants.quickAddMaxNumber,
+//                  macros.proteins < Constants.quickAddMaxNumber else {
+//                return false
+//            }
+//        
+//        return true
+//    }
 }
